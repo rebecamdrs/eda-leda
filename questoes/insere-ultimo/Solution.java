74d11904fbffc296765cf9b17c1ff87c6e9a2ae2
Rebeca <rebecadmsilva@gmail.com>
2026-05-05 18:06:57 -0300

    feat: finalizando questão insere-ultimo

--- questoes/insere-ultimo/Solution.java
+++ questoes/insere-ultimo/Solution.java
@@ -9,6 +9,17 @@ class Solution {
         for (int i = 0; i < v.length; i++) {
             v[i] = Integer.parseInt(seq[i]);
         }
-        System.out.println(Arrays.toString(insereUltimo(v)));
+	insercaoOrdenada(v);
+        System.out.println(Arrays.toString(v));
+    }
+
+    private static void insercaoOrdenada(int[] v) {
+    	for (int i = v.length-1; i > 0; i--) {
+	    if (v[i] < v[i-1]) {
+	    	int temp = v[i];
+		v[i] = v[i-1];
+		v[i-1] = temp;
+	    }
+	}
     }
 }
