8a63a780e983a43dc10821e910f378fbce55aabd
Rebeca <rebecadmsilva@gmail.com>
2026-05-05 18:19:38 -0300

    feat: adicionando questao insere-primeiro

---
+++ questoes/insere-primeiro/Solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+	String[] seq = sc.nextLine().trim().split(" ");
+	int[] v = new int[seq.length];
+	for (int i = 0; i < seq.length; i++) {
+	    v[i] = Integer.parseInt(seq[i]);
+	}
+	insercaoOrdenada(v);
+	System.out.println(Arrays.toString(v));
+    }
+
+    private static void insercaoOrdenada(int[] v) {
+    	for (int i = 0; i < v.length-1; i++) {
+	    if (v[i] > v[i+1]) {
+	    	int temp = v[i];
+		v[i] = v[i+1];
+		v[i+1] = temp;
+	    }
+	}
+    }
+}
